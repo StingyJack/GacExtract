b6a6c1d2ef68ab02850195ed043936a91deb272d
Andrew Stanton <astanton1978@hotmail.com>
2016-12-25 11:59:36 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new Process();
+            //your path may vary
+            process.StartInfo.FileName = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\gacutil.exe";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.Arguments = "/l";
+            process.Start();
+
+            var consoleOutput = process.StandardOutput;
+
+
+            var assemblyList = new List<string>();
+            var startAdding = false;
+            while (consoleOutput.EndOfStream == false)
+            {
+                var line = consoleOutput.ReadLine();
+                if (line.IndexOf("The Global Assembly Cache contains the following assemblies", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    startAdding = true;
+                    continue;
+                }
+
+                if (startAdding == false)
+                {
+                    continue;
+                }
+
+                //add any other filter conditions (framework version, etc)
+                if (line.IndexOf("System.", StringComparison.OrdinalIgnoreCase) < 0)
+                {
+                    continue;
+                }
+
+                assemblyList.Add(line.Trim());
+            }
+
+            var collectedRecords = new List<string>();
+            var failedToLoad = new List<string>();
+
+            Console.WriteLine($"Found {assemblyList.Count} assemblies");
+            var currentItem = 1;
+
+
+            foreach (var gacAssemblyInfo in assemblyList)
+            {
+                Console.SetCursorPosition(0, 2);
+                Console.WriteLine($"On {currentItem} of {assemblyList.Count} ");
+                Console.SetCursorPosition(0, 3);
+                Console.WriteLine($"Loading {gacAssemblyInfo}");
+                currentItem++;
+
+                try
+                {
+                    var asm = Assembly.Load(gacAssemblyInfo);
+
+                    foreach (Type t in asm.GetTypes())
+                    {
+                        if (t.Name.EndsWith("Attribute", StringComparison.OrdinalIgnoreCase))
+                        {
+                            collectedRecords.Add($"{t.FullName} - {t.Assembly.FullName}");
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    failedToLoad.Add($"FAILED to load {gacAssemblyInfo} - {ex}");
+                    Console.SetCursorPosition(1, 9);
+                    Console.WriteLine($"Failure to load count: {failedToLoad.Count}");
+                    Console.SetCursorPosition(4, 10);
+                    Console.WriteLine($"Last Fail: {gacAssemblyInfo}");
+                }
+            }
+
+            var fileBase = System.IO.Path.GetRandomFileName();
+            var goodFile = $"{fileBase}_good.txt";
+            var failFile = $"{fileBase}_failedToLoad.txt";
+            System.IO.File.WriteAllLines(goodFile, collectedRecords);
+            System.IO.File.WriteAllLines(failFile, failedToLoad);
+            Console.SetCursorPosition(0, 15);
+            Console.WriteLine($"Matching types: {goodFile}");
+            Console.WriteLine($"Failures: {failFile}");
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
